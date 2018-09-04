e470275afbcf6ca595128bcdb304881b1473d056
anil093 <anil.kumar093@outlook.com>
2018-09-04 16:23:06 -0400

    files updated

--- bamazon.sql
+++ bamazon.sql
@@ -1,13 +1,19 @@
 create table products(
     itemid int primary key AUTO_INCREMENT,
-    product_name varchar (50),
-    department_name varchar(50),
-    price float(10),
-    stock_quantity float(20)
+    product_name varchar
+(50),
+    department_name varchar
+(50),
+    price float
+(10),
+    stock_quantity float
+(20)
 );
 
-insert into  products (Product_name,department_name,price,stock_quantity) values
-("clamp","dept a","workbench clamp",12,5),
-("screwdriver","dept b","flat head",3,10),
-("tire bar","dept c","tool for changing tries", 15, 10),
-("3mm bracket","dept d","Mechanic",67,4);
+insert into  products
+    (Product_name,department_name,price,stock_quantity)
+values
+    ("clamp", "workbench clamp", 12, 5),
+    ("screwdriver", "flat head", 3, 10),
+    ("tire bar", "tool for changing tries", 15, 10),
+    ("3mm bracket", "Mechanic", 67, 4);
